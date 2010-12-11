1295de95e8e7f68842c4ffc2c26af33e222c0d2a
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2010-12-11 19:04:10 +0200

    Added AR associations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,4 @@
 class Campaign < ActiveRecord::Base
+  has_and_belongs_to_many :places
   attr_accessible :name, :action, :expire_at
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
   belongs_to :business
+  has_and_belongs_to_many :campaigns
   attr_accessible :name, :long, :lat, :description
 end
