ac5791b8fd30608e15ca85044e877067cb4eca02
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2011-01-18 22:37:59 +0200

    Added capistrano settings and DB specific settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :user, 'kazdoor'  # Your dreamhost account's username
+set :domain, 'ps41154.dreamhostps.com'  # Dreamhost servername where your account is located 
+set :project, 'spinninghats.com'  # Your application as its called in the repository
+set :application, 'spinninghats.com'  # Your app's location (domain or sub-domain name as setup in panel)
+set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# deploy config
+set :deploy_to, applicationdir
+set :deploy_via, :export
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+# additional settings
+default_run_options[:pty] = true  # Forgo errors when deploying from windows
+#ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :use_sudo, false
+ssh_options[:forward_agent] = true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm, 'git'
+set :repository,  "git@github.com:Kazdoor/monopoly.git"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1 # if you have vendored rails
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
