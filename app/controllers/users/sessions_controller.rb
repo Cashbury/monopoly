89b74955bd1d91720b025c878cde5ef2be3f0a82
Basayel Said <basayel.said@espace.com.eg>
2011-03-27 13:27:47 +0200

    fix bugs

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,17 +5,24 @@ class Users::SessionsController < Devise::SessionsController
 			format.xml { #request from iphone 
 				@user=User.find_by_email(params[:email]) 
 				is_valid_user=!@user.nil? && @user.valid_password?(params[:password])
-				if is_valid_user && !params[:email].match(/\b@facebook.com.fake\b/).nil?
-    			@user = User.new(:email=>params[:email],
-                           :password=>params[:password],
-                           :password_confirmation =>params[:password],
-                           :full_name=>params[:full_name])
-					@user.ensure_authentication_token!
-					if @user.save!
+				if !params[:email].match(/\b@facebook.com.fake\b/).nil? #login from facebook
+					if @user.nil? #new account
+    				@user = User.new(:email=>params[:email],
+                           	 :password=>params[:password],
+                             :password_confirmation =>params[:password],
+                             :full_name=>params[:full_name])
+						@user.ensure_authentication_token!
+						if @user.save!
+							sign_in @user
+							render :xml => current_user.to_xml(:only=>[:id,:email,:full_name,:authentication_token]),:status=>200
+						else
+							render :xml => {:error=>@user.errors.full_messages.join(',')},:status=>200
+						end
+					elsif is_valid_user
 						sign_in @user
 						render :xml => current_user.to_xml(:only=>[:id,:email,:full_name,:authentication_token]),:status=>200
 					else
-						render :xml => {:error=>@user.errors.full_messages.join(',')},:status=>200
+						render :xml => {:error=>'Invalid email/password'},:status=>200
 					end
       	elsif !is_valid_user
       		render :xml => {:error=>"Invalid email/password"},:status=>200
