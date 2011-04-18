1631e87e669ab64b39d74ddf862d356ddb6140c0
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2011-04-18 15:38:48 +0200

    Adding phone to Place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418131413) do
+ActiveRecord::Schema.define(:version => 20110418133534) do
 
   create_table "account_holders", :force => true do |t|
     t.string   "model_type"
@@ -279,6 +279,7 @@ ActiveRecord::Schema.define(:version => 20110418131413) do
     t.string   "zipcode"
     t.string   "country"
     t.string   "distance",                                     :default => "0"
+    t.string   "phone"
   end
 
   add_index "places", ["business_id"], :name => "index_places_on_business_id"
