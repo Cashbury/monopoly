28a86567526d0858c0f64b40b13173ad5d30ef12
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2011-04-27 12:35:22 +0200

    Deleting the Relation many to many between Reward and Places, instead we have many to many relation bbetween campiagn and rewards

--- app/models/place.rb
+++ app/models/place.rb
@@ -21,7 +21,6 @@ class Place < ActiveRecord::Base
   belongs_to :address
   
   has_and_belongs_to_many :engagements
-  has_and_belongs_to_many :rewards
   has_and_belongs_to_many :amenities
   
   has_many :qr_codes,:as=>:associatable
