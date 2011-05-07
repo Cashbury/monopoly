afd6b2503cbd3f8e9756bd7951f68abf29d4f36e
Basayel Said <eng.basayel.said@gmail.com>
2011-05-08 02:58:10 +0300

    modifying claiming a reward

--- app/controllers/users/rewards_controller.rb
+++ app/controllers/users/rewards_controller.rb
@@ -1,37 +1,31 @@
 class Users::RewardsController < Users::BaseController
 	def claim
-		begin
+		#begin
 			reward=Reward.find(params[:id])
 			if reward.campaign.nil?
 				respond_with_error(ERRORS[:msg_not_related_to_campaign])
-			elsif (account=current_user.account_holder.accounts.where(:campaign_id=>reward.campaign.id).first).blank?
+			elsif (user_account=reward.campaign.user_account(current_user)).blank?
 				respond_with_error(ERRORS[:msg_user_not_enrolled])
 			else
-		    if account.amount < reward.needed_amount 
+		    if user_account.amount < reward.needed_amount 
 		    	respond_with_error(ERRORS[:msg_not_enough_credit])
 				else
-					number_of_times=Log.redeems_logs.where(:user_id=>current_user.id,:reward_id=>reward.id).count
-					if !reward.claim.nil? && number_of_times >=reward.claim
-						respond_with_error(ERRORS[:msg_exceeds_claim_limit])
-					else
-						reward.is_claimed_by(current_user,account,params[:place_id],params[:lat],params[:lng])
-						respond_to do |format|
-		      		format.xml { render :xml => reward_hash(reward,account,reward.campaign),:status=>200 }
-		    		end
-					end
+          reward.is_claimed_by(current_user,user_account,params[:place_id],params[:lat],params[:lng])
+          respond_to do |format|
+            format.xml { render :xml => reward_hash(reward,user_account,reward.campaign),:status=>200 }
+          end
 				end
 			end
-		rescue Exception => e
-			logger.error "Exception #{e.class}: #{e.message}"
-			respond_with_error(e.message)
-		end
+		# rescue Exception => e
+		# 	logger.error "Exception #{e.class}: #{e.message}"
+		# 	respond_with_error(e.message)
+		# end
 	end
 	
 	ERRORS={
   	:msg_not_related_to_campaign=>"Reward not associated to campaign",
   	:msg_user_not_enrolled      =>"User not enrolled with the reward's campaign",
-  	:msg_not_enough_credit      =>"Not enough credit",
-  	:msg_exceeds_claim_limit    =>"Reward reached max mumber of claims"
+  	:msg_not_enough_credit      =>"Not enough credit"
   }
   
   private
@@ -40,7 +34,7 @@ class Users::RewardsController < Users::BaseController
 		r[:redeem].merge!({:business_id      => campaign.program.business.id})
 		r[:redeem].merge!({:campaign_id      => campaign.id})
 		r[:redeem].merge!({:reward_id        => reward.id})
-		r[:redeem].merge!({:account_points   => account.amount})
+		r[:redeem].merge!({:account_amount   => account.amount})
     r
   end
   
