404a0bb3c7f57f509434802226a2bead7c7d9dfe
Rajib Ahmed <cool_rajib@hotmail.com>
2011-07-20 10:11:41 +0200

    country profile updates

---
+++ db/migrate/20110718011923_add_columns_to_cities.rb
@@ -0,0 +1,9 @@
+class AddColumnsToCities < ActiveRecord::Migration
+  def self.up
+    add_column :cities, :state_id, :integer
+  end
+
+  def self.down
+    remove_column :cities, :state_id
+  end
+end
