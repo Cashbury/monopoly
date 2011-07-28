5123676c562f4c1827ccc3c6e91bd4570bc10020
Basayel Said <eng.basayel.said@gmail.com>
2011-07-28 20:08:44 +0200

    fixing issues

--- app/controllers/users/cashiers_controller.rb
+++ app/controllers/users/cashiers_controller.rb
@@ -17,35 +17,46 @@ class Users::CashiersController < Users::BaseController
   end
   
   def ring_up
-    qr_code=QrCode.associated_with_users.where(:hash_code=>params[:customer_identifier]).first
-    user=qr_code.user if qr_code.present?
-    employee=current_user.employees.where(:role_id=>Role.find_by_name(Role::AS[:cashier]).id).first   
-    business=Business.find(employee.business_id)
-    user_type=user.engaged_with_business?(business) ? "Returning Customer" : "New Customer"
-    #Loyalty collect campaigns
-    unless params[:engagements].blank?
-      params[:engagements].each do |record| 
-        records=record.split(',')
-        engagement_id=records.first;quantity=records.second
-        engagement=Engagement.find(engagement_id)
-        user.engaged_with(engagement,nil,nil,params[:lat],params[:long],"User made an engagement through cashier",quantity.to_i)
+    begin
+      qr_code=QrCode.associated_with_users.where(:hash_code=>params[:customer_identifier]).first
+      if qr_code.present? and qr_code.status #active
+        user=qr_code.user
+        employee=current_user.employees.where(:role_id=>Role.find_by_name(Role::AS[:cashier]).id).first   
+        business=Business.find(employee.business_id)
+        user_type=user.engaged_with_business?(business) ? "Returning Customer" : "New Customer"
+        #Loyalty collect campaigns
+        unless params[:engagements].blank?
+          params[:engagements].each do |record| 
+            records=record.split(',')
+            engagement_id=records.first;quantity=records.second
+            engagement=Engagement.find(engagement_id)
+            user.engaged_with(engagement,nil,nil,params[:lat],params[:long],"User made an engagement through cashier",quantity.to_i)
+          end
+        end
+        #Spend based campaign    
+        campaign=business.spend_based_campaign
+        user.made_spend_engagement_at(business,campaign,params[:amount].to_f,params[:lat],params[:lng])
+        user.issue_qrcode(current_user.id, qr_code.size, qr_code.code_type)
+        qr_code.scan
+        
+        s = {}
+		    s.merge!({:amount             => params[:amount]})
+		    s.merge!({:currency_symbol    => ISO4217::Currency.from_code(business.currency_code).symbol})
+		    s.merge!({:customer_name      => user.full_name})
+		    s.merge!({:customer_type      => user_type})
+		    user_uid=user.email.split("@facebook").first
+        s.merge!({:customer_image_url => URI.escape(user.email.match(/facebook/) ? "https://graph.facebook.com/#{user_uid}/picture" : "/images/user-default.jpg")})
+        respond_to do |format|     
+          format.xml {render :xml => s , :status => 200}
+        end
+      else
+        respond_to do |format|     
+          format.xml {render :text => "Invalid Qrcode"  , :status => 500}
+        end
       end
-    end
-    #Spend based campaign    
-    campaign=business.spend_based_campaign
-    user.made_spend_engagement_at(business,campaign,params[:amount].to_f,params[:lat],params[:lng])
-    user.issue_qrcode(current_user.id, qr_code.size, qr_code.code_type)
-    qr_code.scan
-    
-    s = {}
-		s.merge!({:amount             => params[:amount]})
-		s.merge!({:currency_symbol    => ISO4217::Currency.from_code(business.currency_code).symbol})
-		s.merge!({:customer_name      => user.full_name})
-		s.merge!({:customer_type      => user_type})
-		user_uid=user.email.split("@facebook").first
-    s.merge!({:customer_image_url => URI.escape(user.email.match(/facebook/) ? "https://graph.facebook.com/#{user_uid}/picture" : "/images/user-default.jpg")})
-    respond_to do |format|     
-      format.xml {render :xml => s , :status => 200}
+    rescue Exception=>e
+      logger.error "Exception #{e.class}: #{e.message}"
+      render :text => e.message, :status => 500
     end
   end
   
