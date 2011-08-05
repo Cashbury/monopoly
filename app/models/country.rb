55f49495ad0d7652cfc897b0aa9d5e9b2f9694c4
Rajib Ahmed <cool_rajib@hotmail.com>
2011-08-05 07:18:07 +0200

    Added icon and its migration file

--- app/models/country.rb
+++ app/models/country.rb
@@ -13,4 +13,9 @@ class Country < ActiveRecord::Base
   has_many :cities
   has_many :addresses
   has_one  :address_profile
+  has_attached_file :currency_icon , :styles=>{
+                                        :small=>"8x8",
+                                        :medium=>"16x16",
+                                        :large=>"32x32"
+                                      }
 end
