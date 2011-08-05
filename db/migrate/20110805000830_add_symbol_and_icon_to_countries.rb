b231fef4038a70861a88775dd2794229cdc0b087
Rajib Ahmed <cool_rajib@hotmail.com>
2011-08-05 02:51:20 +0200

    migration file fix

--- db/migrate/20110805000830_add_symbol_and_icon_to_countries.rb
+++ db/migrate/20110805000830_add_symbol_and_icon_to_countries.rb
@@ -1,11 +1,9 @@
 class AddSymbolAndIconToCountries < ActiveRecord::Migration
   def self.up
-    #add_column :countries, :currency_symbol, 	:string
     add_column :countries, :currency_icon , 	:string
   end
 
   def self.down
     remove_column :countries, :currency_icon
-    remove_column :countries, :currency_symbol
   end
 end
