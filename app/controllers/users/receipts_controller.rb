e7696f224245a8bcd32d77ec29630987379c1989
Basayel Said <eng.basayel.said@gmail.com>
2011-08-08 11:28:59 +0200

    fixing bugs

--- app/controllers/users/receipts_controller.rb
+++ app/controllers/users/receipts_controller.rb
@@ -1,6 +1,6 @@
 class Users::ReceiptsController < Users::BaseController
   def index
-    all_receipts=current_user.receipts.joins([:business,:place=>:address]).select("receipts.amount,receipts.business_id, receipts.place_id, receipts.receipt_text,receipts.receipt_type, receipts.user_id, receipts.transaction_id, receipts.created_at as date_time, places.name as place_name, businesses.name as business_name")
+    all_receipts=current_user.receipts.joins([:business=>:brand,:place=>:address]).select("receipts.amount,receipts.business_id, receipts.place_id, receipts.receipt_text,receipts.receipt_type, receipts.user_id, receipts.transaction_id, receipts.created_at as date_time, places.name as place_name, brands.name as brand_name")
     result={}
     all_receipts.each do |receipt|
       result[:receipt]=receipt.attributes
