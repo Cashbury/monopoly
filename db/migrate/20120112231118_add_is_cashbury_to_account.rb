3145b1f723d59eae6b834d53d19d15dce17b69e3
Arron Washington <arron@theradicaledwards.com>
2012-01-12 18:40:32 -0500

    Automatically create cashburies account for money program.

---
+++ db/migrate/20120112231118_add_is_cashbury_to_account.rb
@@ -0,0 +1,13 @@
+class AddIsCashburyToAccount < ActiveRecord::Migration
+  def self.up
+    change_table :accounts do |t|
+      t.boolean :is_cashbury, :default => false
+    end
+  end
+
+  def self.down
+    change_table :accounts, do |t|
+      t.remove :is_cashbury
+    end
+  end
+end
