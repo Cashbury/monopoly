0daf340ddc0f61f215061e82e445172510f57095
Arron Washington <arron@theradicaledwards.com>
2012-01-14 18:39:12 -0500

    Can void group of transactions.

--- spec/models/transaction_group_spec.rb
+++ spec/models/transaction_group_spec.rb
@@ -7,4 +7,29 @@ describe TransactionGroup do
 
     tx.friendly_id.should be_present
   end
+
+  it "should void all transactions in its group" do
+    business = Factory(:business)
+    user = Factory(:consumer)
+    op  = Factory(:cashbury_operator)
+
+    business.create_money_program!
+    user.enroll(business.money_program)
+
+    account = user.cash_account_for(business)
+    tx_grp = Account.group_transactions do
+      account.load(50)
+      account.spend(50)
+    end
+
+    tx_grp.transactions.should have(2).items
+
+    tx_grp.void!(op)
+
+    tx_grp.transactions.reload
+    tx_grp.transactions.each do |tx|
+      tx.state.should == Transaction::States::VOID
+    end
+
+  end
 end
