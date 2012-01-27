04fee5ec122157220d8efe8620e81816c460f2e8
Arron Washington <arron@theradicaledwards.com>
2012-01-27 18:12:03 -0500

    Ensure account holder model exists.

--- app/models/user.rb
+++ app/models/user.rb
@@ -220,7 +220,7 @@ class User < ActiveRecord::Base
   end
 
   def cash_account_for(business)
-    return nil if !self.account_holder
+    ensure_account_holder!
     money_program = business.money_program
     Account.where(:business_id => business.id)
       .where(:program_id => money_program.id)
@@ -230,7 +230,7 @@ class User < ActiveRecord::Base
   end
 
   def cashbury_account_for(business)
-    return nil if !self.account_holder
+    ensure_account_holder!
     money_program = business.money_program
     Account.where(:business_id => business.id)
       .where(:program_id => money_program.id)
@@ -240,6 +240,7 @@ class User < ActiveRecord::Base
   end
 
   def cashbury_accounts
+    ensure_account_holder!
     Account.where(:is_cashbury => true)
       .where(:account_holder_id => self.account_holder.id)
   end
@@ -609,4 +610,9 @@ class User < ActiveRecord::Base
     phone_number.gsub(/^#{Regexp.escape(code)}/, '')
   end
 
+  protected
+
+  def ensure_account_holder!
+    self.create_account_holder if account_holder.blank?
+  end
 end
