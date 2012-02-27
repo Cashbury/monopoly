f67a77a907d466313162235097b01e0608d19cfa
Amer Jazaerli <amer.jazaerly@gmail.com>
2012-02-27 22:57:07 +0100

    Reinstall and Reconfigure Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,44 @@
-load 'config/deploy/settings.rb'
-load 'config/deploy/symlinks.rb'
-load 'config/deploy/passenger.rb'
-load 'config/deploy/callbacks.rb'
-load 'config/deploy/maintenance.rb'
-load 'config/deploy/git.rb'
+# RVM bootstrap
+$:.unshift(File.expand_path("~/.rvm/lib"))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.2-p290'
+set :rvm_type, :user
+
+# bundler bootstrap
+require 'bundler/capistrano'
+
+# main details
+set :application, "cashnode.cashbury.com"
+set :domain, application
+role :web, domain   # Your HTTP server, Apache/etc
+role :app, domain   # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/www/apps/#{domain}"
+set :deploy_via, :remote_cache
+set :user, "passenger"
+set :use_sudo, false
+
+# repo details
+set :scm, :git
+set :scm_username, "amer"
+set :repository, "git@gitserver:domainname.git"
+set :branch, "demo2"
+set :git_enable_submodules, 1
+
+# Passenger
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do ; end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
